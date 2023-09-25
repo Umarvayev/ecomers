b7ee677853a0bc863ca4264b90e1ee584165c0d2
Umarvayev <umarvayevxusan@gmail.com>
2023-09-25 12:08:52 +0500

    Category delete part

--- resources/views/admin/allcategory.blade.php
+++ resources/views/admin/allcategory.blade.php
@@ -33,7 +33,7 @@
                             <td>{{ $category->slug}}</td>
                             <td>
                                 <a href="{{ route('editcategory', $category->id)}}" class="btn btn-primary">Edit</a>
-                                <a href="" class="btn btn-danger">Delete</a>
+                                <a href="{{ route('deletecategory', $category->id)}}" class="btn btn-danger">Delete</a>
                             </td>
                         </tr>
                         @endforeach
